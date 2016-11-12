73571c7b74c8b3e662ee90b615c3a60defd941c8
rasca0027 <rasca0027@gmail.com>
2016-11-12 03:13:39 -0500

    fix top rank

--- Kelly and Najma CC_6/Untitled/sketch.js
+++ Kelly and Najma CC_6/Untitled/sketch.js
@@ -1,12 +1,13 @@
-var top, count;
-var d = {};
-var stopwords = ["the",  "a", "one" , "about" , "am" , "he" , "she" , "in" , "out" , "at" , "with" , "is" , "for" , "him" , "her" , "all" , "and" , "to" , "too", "from" , "on" , "are" , "has" , "as" , "two" , "of" , "that"];
+var d = {}; //dictionary
+var stopwords;
+//var stopwords = ["the", "a", "one", "about", "am", "he", "she", "in", "out", "at", "said", "was", "by", "mr", "an", "his", "with", "is", "for", "him", "her", "all", "and", "to", "too", "from", "on", "are", "has", "as", "two", "of" , "that"];
 var font;
-var limit = 0;
+var sorted_dictionary;
 
 function preload() {
   story = loadStrings("text/1.txt");
   font = loadFont("font/writer.ttf");
+  stopwords = loadStrings("text/stopwords.txt");
 }
 
 function setup() {
@@ -15,17 +16,19 @@ function setup() {
   frameRate(1);
   textFont(font);
   textSize(28);
+  
+  // parsing data
   for (var i=0; i< story.length; i++) {
     var l = story[i];
     l = l.toLowerCase(); //makes all words lowercase
     
-    // get rid of symbols
-    l = l.replace(/[.[,?/\\\]@\d;:!-]+/g, '');
-    
+    // get rid of symbols and digits
+    l = l.replace(/[.[,?/\]@\\\d;:!-]+/g, '');
+    // split token
     l = l.split(' ');
     
     for (var j=0; j<l.length; j++) {
-      
+      // get rid of stopwords
       if (stopwords.indexOf(l[j]) >= 0) {
         continue;
       }
@@ -35,42 +38,45 @@ function setup() {
       } else {
         d[l[j]] = 1;
       }
-    }
-  }
+    } // end for
+  } // end for
   console.log(d);
+  
+  // sort them according to frequency
+  sorted_dictionary = Object.keys(d).sort(function(a,b){return d[a]-d[b]});
 
-//this finds the most frequent word
-count = 0;
-  for (var word in d) {
-    console.log(word);
-    if (d.hasOwnProperty(word)) { 
-      if (d[word] > count) {
-        count = d[word];
-        top = word;
-      }
-    }
-  }
-  
-  
 }
 
 
 
 function draw() {
+  var top5 = [];
+  
+  textSize(48);
+  for (var i=2;i<=6;i++) {
+    var word = sorted_dictionary[sorted_dictionary.length - i];
+    text(word, 200, 100 + i*60);
+    top5.push(word);
+  }
+  
 
+  textSize(20);
   //displays text
   for (var word in d) {
-  
+    if (top5.indexOf(word) >= 0)
+      continue;
     if (d.hasOwnProperty(word)) {
-      if (d[word] > 20) {
+      if (d[word] > 10) {
       
        var x = random(600);
        var y = random(600);
+       if ((x < 400) && (x > 100) && (y>150) && (y<500)) {
+         continue;
+       }
        text(word, x, y);
       }
     }
-
-  }
+  } // end for
     
 
   //text(d["visit"], 100, 100);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cc_ex6
+---
+
+### Motivation
+We pick several different topics articles from 3 different new sites.  
+We want to analyze the articles and find out what's the top popular words in news.  
+
+### What we've done
+1. make them lowercase
+2. get rid of symbols and digits
+3. get rid of stopwords. (stopwords list from: http://www.ranks.nl/stopwords)
+4. split the tokens by space
+5. construct a dictionary and word counts
+6. find out the top 5 popular words
+7. display the top 5 words in the middle of canvas
+8. display other words around them
+
+
